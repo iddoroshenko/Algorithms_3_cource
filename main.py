b442b9cfac1f440cb0d94bd5d399b464cf98c084
Ilya Doroshenko <ilya.dor201232@gmail.com>
2021-04-13 22:12:35 +0300

    added Rabin-Karp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import search_string.native as native
+import search_string.rabin_karp as rabin_karp
 
 if __name__ == '__main__':
     print('Hi Anya and Sasha')
     print(native.search("qwerty", "ert"))
+    print(rabin_karp.search("qwerty", "ert"))
---
+++ search_string/rabin_karp.py
@@ -0,0 +1,27 @@
+def search(text, pattern):
+    text_size = len(text)
+    pattern_size = len(pattern)
+    if pattern_size == 0:
+        return 0
+    if text_size == 0 or pattern_size > text_size:
+        return -1
+    circle_module = 1000000000 + 7
+    prime_number = 1007
+    pattern_hash = 0
+    prime_number_step = 1
+    for i in range(pattern_size):
+        pattern_hash = (pattern_hash + prime_number_step * ord(pattern[i])) % circle_module
+        prime_number_step *= prime_number
+    text_hash = 0
+    prime_number_step = 1
+    for i in range(pattern_size):
+        text_hash = (text_hash + prime_number_step * ord(text[i])) % circle_module
+        prime_number_step *= prime_number
+
+    for i in range(0, text_size - pattern_size + 1):
+        if text_hash == pattern_hash:
+            return i
+        if i != text_size - pattern_size:
+            text_hash = (text_hash - ord(text[i])) / prime_number
+            text_hash += ord(text[i+pattern_size]) * prime_number**(pattern_size-1)
+    return -1
