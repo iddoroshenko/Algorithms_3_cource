962bb4e3ed719e0973438d162be0dad6394587b5
Ilya Doroshenko <ilya.dor201232@gmail.com>
2021-06-13 17:25:09 +0300

    fix algos

--- lab-5/fire.py
+++ lab-5/fire.py
@@ -32,6 +32,8 @@ def fitness(cells_machines, cells_details, a, num_of_cells):
                 if a[machines][detail] == 0:
                     n_0_in += 1
     n_1_out -= n_1
+    if (n_1 + n_0_in) == 0:
+        return
     return (n_1 - n_1_out) / (n_1 + n_0_in)
 
 
@@ -53,17 +55,45 @@ def single_move(current_solve, a, num_of_cells):
             cells_details[i].append(detail_to_move)
             new_cells_machine = machines_cells_selecting(a, cells_details, num_of_cells)
             new_f = fitness(new_cells_machine, cells_details, a, num_of_cells)
-            if new_f >= best_f:
-                best_cells_machine = new_cells_machine
-                best_cells_details = cells_details
+            if new_f > best_f:
+                best_cells_machine = new_cells_machine.copy()
+                best_cells_details = cells_details.copy()
                 best_f = new_f
             cells_details[i].remove(detail_to_move)
     cells_details[old_cell].append(detail_to_move)
     return best_f, best_cells_details, best_cells_machine
 
 
-def exchange_move():
-    pass
+def exchange_move(current_solve, a, num_of_cells):
+    n = len(a[0])  # число деталей
+    cells_details = current_solve[1].copy()
+    best_f = -9999
+    best_cells_details = []
+    best_cells_machine = []
+    detail_to_move = random.randint(0, n - 1)
+    old_cell = 0
+    for i in range(len(cells_details)):
+        if detail_to_move in cells_details[i]:
+            old_cell = i
+            cells_details[old_cell].remove(detail_to_move)
+            break
+    for i in range(len(cells_details)):
+        if i != old_cell and len(cells_details[i]) != 0:
+            detail_to_move2 = cells_details[i][random.randint(0, len(cells_details[i]) - 1)]
+            cells_details[i].append(detail_to_move)
+            cells_details[i].remove(detail_to_move2)
+            cells_details[old_cell].append(detail_to_move2)
+            new_cells_machine = machines_cells_selecting(a, cells_details, num_of_cells)
+            new_f = fitness(new_cells_machine, cells_details, a, num_of_cells)
+            if new_f > best_f:
+                best_cells_machine = new_cells_machine.copy()
+                best_cells_details = cells_details.copy()
+                best_f = new_f
+            cells_details[i].remove(detail_to_move)
+            cells_details[i].append(detail_to_move2)
+            cells_details[old_cell].remove(detail_to_move2)
+    cells_details[old_cell].append(detail_to_move)
+    return best_f, best_cells_details, best_cells_machine
 
 
 def machines_cells_selecting(a, cells_details, num_of_cells):
@@ -78,9 +108,9 @@ def machines_cells_selecting(a, cells_details, num_of_cells):
                 else:
                     for i in range(num_of_cells):
                         if i != cell:
-                            for detail2 in cells_details[cell]:
-                                if a[machine][detail2] == 1:
-                                    error[i] += 1
+                            #for detail2 in cells_details[cell]:
+                                #if a[machine][detail2] == 1:
+                            error[i] += 1
 
         ans = np.argmin(error)
         cells_machines[ans].append(machine)
@@ -88,10 +118,6 @@ def machines_cells_selecting(a, cells_details, num_of_cells):
     return cells_machines
 
 
-#############
-
-
-#############
 def get_ans(a):
     num_of_cells = 2
 
@@ -107,28 +133,41 @@ def get_ans(a):
             if b[i][j] != 0:
                 similarity.append((b[i][j], i, j))
     similarity.sort(reverse=True)
+    best_solve = [-9, [], []]
 
-    delta = similarity[int(math.sqrt(len(similarity)))][0]
-    cells_details = [[] for x in range(num_of_cells)]
-    cells_details[0].append(similarity[0][1])
-    cells_details[0].append(similarity[0][2])
-    for i in range(1, len(similarity)):
-        if similarity[i][0] > delta:
-            if similarity[i][1] not in cells_details[0]:
-                cells_details[0].append(similarity[i][1])
-            if similarity[i][2] not in cells_details[0]:
-                cells_details[0].append(similarity[i][2])
-    for detail in range(n):
-        if detail not in cells_details[0]:
-            cells_details[1].append(detail)
-    cells_machines = machines_cells_selecting(a, cells_details, num_of_cells)
-
-    best_solve = (fitness(cells_machines, cells_details, a, num_of_cells), cells_details, cells_machines)  # S**
-    current_best_solve = best_solve  # S*
-    current_solve = best_solve  # S
-    goto_2 = True
+    goto_1 = True
     while True:
-        if goto_2:
+        if goto_1:
+            #delta = similarity[int(math.sqrt(len(similarity)))][0]
+            #delta = [1/(x+2) for x in range(num_of_cells - 1)]
+            delta = [similarity[(x+1)*int(math.sqrt(len(similarity)))][0] for x in range(num_of_cells - 1)]
+            cells_details = [[] for x in range(num_of_cells)]
+            cells_details[0].append(similarity[0][1])
+            cells_details[0].append(similarity[0][2])
+            delta_counter = 0
+            for i in range(1, len(similarity)):
+                if similarity[i][0] > delta[delta_counter]:
+                    if similarity[i][1] not in cells_details[delta_counter]:
+                        cells_details[delta_counter].append(similarity[i][1])
+                    if similarity[i][2] not in cells_details[delta_counter]:
+                        cells_details[delta_counter].append(similarity[i][2])
+                else:
+                    delta_counter += 1
+                if delta_counter == num_of_cells - 1:
+                    break
+            for detail in range(n):
+                f = True
+                for i in range(num_of_cells-1):
+                    if detail in cells_details[i]:
+                        f = False
+                if f:
+                    cells_details[num_of_cells - 1].append(detail)
+            cells_machines = machines_cells_selecting(a, cells_details, num_of_cells)
+
+            current_best_solve = (fitness(cells_machines, cells_details, a, num_of_cells), cells_details, cells_machines)  # S**
+            if current_best_solve[0] > best_solve[0]:
+                best_solve = current_best_solve  # S*
+            current_solve = current_best_solve  # S
             T_0 = 1
             T_f = 0.2
             T = T_0
@@ -140,11 +179,11 @@ def get_ans(a):
             counter_trapped = 0
             counter_stagnant = 0
 
-        goto_2 = False
+        goto_1 = False
         while counter_mc < L and counter_trapped < L / 2:
             new_solve = single_move(current_solve, a, num_of_cells)  # S_c
-            #if counter % D == 0:
-            #    cells_solve = exchange_move(current_solve)
+            if counter % D == 0:
+                new_solve = exchange_move(current_solve, a, num_of_cells)
             if new_solve[0] > current_best_solve[0]:
                 current_best_solve = new_solve
                 counter_stagnant = 0
@@ -167,9 +206,7 @@ def get_ans(a):
             if current_best_solve[0] > best_solve[0]:
                 best_solve = current_best_solve
                 num_of_cells += 1
-                goto_2 = True
-                current_solve[1].append([])
-                current_solve[2].append([])
+                goto_1 = True
             else:
                 break
         else:
--- lab-5/main.py
+++ lab-5/main.py
@@ -1,14 +1,17 @@
-
 import fire
 
 
-if __name__ == '__main__':
-    file = open('benchmarks/20x20.txt', 'r')
+def parse_file(s):
+    file = open(s, 'r')
     lines = file.readlines()
     m, p = map(int, lines[0].split())
     a = [[0 for x in range(p)] for y in range(m)]
     for line in range(1, len(lines)):
         for i in list(map(int, lines[line].split()))[1:]:
-            a[line-1][i-1] = 1
-    for i in range(1000):
-        print(fire.get_ans(a))
+            a[line - 1][i - 1] = 1
+    return a
+
+
+if __name__ == '__main__':
+    for i in ['20x20', '24x40', '30x50', '30x90', '37x53']:
+        print(fire.get_ans(parse_file('benchmarks/' + i + '.txt')))
