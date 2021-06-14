13293b9c3bd7400c01ead195cfedae05905e4735
Baturina Anna <baturinaanya@yandex.ru>
2021-06-14 11:10:39 +0300

    add report and files with answers

--- lab-5/main.py
+++ lab-5/main.py
@@ -1,4 +1,5 @@
 import fire
+import time
 
 
 def parse_file(s):
@@ -12,6 +13,24 @@ def parse_file(s):
     return a
 
 
+def get_line_from_lists(lst):
+    line = ''
+    for l in lst:
+        line += '[' + ' '.join(list(map(str, l))) + ']'
+    return line
+
+
 if __name__ == '__main__':
     for i in ['20x20', '24x40', '30x50', '30x90', '37x53']:
-        print(fire.get_ans(parse_file('benchmarks/' + i + '.txt')))
+        total_time = 0
+        answers = []
+        file = open('answers/' + i + '.sol', 'w')
+        for j in range(10):
+            start = time.time()
+            answers.append(fire.get_ans(parse_file('benchmarks/' + i + '.txt')))
+            total_time += time.time() - start
+        print(f'FILE: {i} - TIME: {total_time/10}')
+        answers = sorted(answers, key=lambda x: x[0], reverse=True)
+        file.write(get_line_from_lists(answers[0][1]) + '\n')
+        file.write(get_line_from_lists(answers[0][2]))
+
